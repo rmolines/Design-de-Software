044703ce4260529b68502dbb3ebe17d97ea562a0
rmolines <rafaelmismael@gmail.com>
2015-03-26 17:39:01 -0300

    RafaelMolines_EP2

--- forca.py
+++ forca.py
@@ -5,30 +5,56 @@ Created on Sun Mar 22 22:16:15 2015
 @author: Rafael
 """
 
+
+from random import randint
+
 import turtle
-
+    
 j = turtle.Screen ()
+j.screensize (640, 480)
 
-def desenho (x=-300, l=60, l2=30, l3=-45):
+text = open ("entrada.txt", encoding = "utf-8")
+
+y = randint (0,11)
+
+t = text.readlines ()
+p = "abc"
+
+d = turtle.Turtle ()
+d.penup ()
+d.setpos (-200, -200)
+d.pendown ()
+
+
+def linhas ():
+            
+    for i in p:
+        d.forward (25)
+        d.penup ()
+        d.forward (5)
+        d.pendown ()
+        
+linhas ()
+
+d.penup ()
+d.back (25*len (p)+5*len (p))
+
+c = turtle.textinput ("Digite seu chute", "Digite seu chute")
+
+if p.find (c) == True:
+    d.write (c)
     
-    c1 = turtle.Turtle ()
-    c1.color ("black")
-    c1.hideturtle ()
-    c1.speed (1000000)
-    c1.penup ()
-    c1.goto (x,-150)
-    c1.pendown ()
-    c1.left (l)
-    c1.forward (120)
-    c1.left (l2)
-    c1.forward (180)
-    c1.right (90)
-    c1.circle (50)
-    c1.left (l3)
-    c1.forward (100)
-    
-desenho ()
-desenho (-180, 120, -30, 225)
-    
+
+        
+        
 j.exitonclick ()
+        
     
+    
+    
+    
+    
+
+
+
+
