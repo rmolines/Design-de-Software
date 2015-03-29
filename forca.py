f9188980c52b341bd0aa6c6d086db47cec1541b3
rmolines <rafaelmismael@gmail.com>
2015-03-29 14:00:25 -0300

    Versao 6

--- forca.py
+++ forca.py
@@ -21,8 +21,9 @@ t = text.readlines ()
 p = "banana"
 
 forca = turtle.Turtle ()
+forca.hideturtle ()
 forca.penup ()
-forca.setpos (-300,-150)
+forca.setpos (-320,-150)
 forca.pendown ()
 forca.left (90)
 forca.forward (400)
@@ -31,13 +32,14 @@ forca.forward (150)
 forca.right (90)
 forca.forward (100)
 
+
 d = turtle.Turtle ()
 
 
 def linhas ():
     
     d.penup ()
-    d.setpos (-200, -200)
+    d.setpos (-320, -300)
     d.pendown ()
             
     for i in p:
@@ -52,14 +54,51 @@ def linhas ():
         
 linhas ()
 
-def desenharforca ():
-    
+def desenharforca (e):
+    if e == 1:
+        forca.right (90)
+        forca.circle (30)
+        forca.penup ()
+        forca.left (90)
+        forca.forward (60)
+        forca.pendown ()
+    elif e == 2:
+        forca.forward (100)
+        forca.back (100)
+    elif e == 3:
+        forca.right (30)
+        forca.forward (50)
+        forca.back (50)
+    elif e == 4:
+        forca.left (60)
+        forca.forward (50)
+        forca.back (50)
+        forca.penup ()
+        forca.right (30)
+        forca.forward (100)
+        forca.pendown ()
+    elif e == 5:
+        forca.right (20)
+        forca.forward (60)
+        forca.back (60)
+    elif e == 6:
+        forca.left (40)
+        forca.forward (60)
 
 letras = 0
-    
+erros = 0 
+
+x = turtle.Turtle ()
+x.penup ()
+x.hideturtle ()
+x.goto (100, 100)
+
+chutes = ""
 
 def acharletra (c):
     
+    global chutes
+    global erros
     global letras    
     
     y = 0
@@ -69,9 +108,18 @@ def acharletra (c):
         d.back (p.find (c, y)*25+p.find(c, y)*5)
         y += p.find (c)+1
         letras += 1
-    
         
-            
+    if p.find (c) == -1:
+        if chutes.find (c.upper ()) == -1:        
+            erros += 1        
+            desenharforca (erros)
+            x.write (c.upper (), font= ("Arial", 20))
+            x.forward (20)
+            chutes += c.upper ()
+            print (chutes.find (c))
+        else:
+            x.write ("Voce ja chutou esta letra")
+        
     
         
 def acertarpalavra ():   
@@ -80,7 +128,11 @@ def acertarpalavra ():
     while letras < len (p):
       
         chute = turtle.textinput ("Digite seu chute", "Digite seu chute")
-        acharletra (chute)
+        acharletra (chute[0])
+    if letras == len (p):
+        x = turtle.Turtle ()
+        x.hideturtle ()
+        x.write ("Parabens!\nVoce ganhou!\nDeseja jogar novamente?", font= ("Arial", 35))
     
 acertarpalavra () 
 
